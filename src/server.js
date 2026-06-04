dd560449bfcf7dc88b4e662f55f6dc909b637c07
CT23A124 <ngwaruti16@gmail.com>
2026-06-04 07:44:10 +0100

    Fix CORS for Lovable frontend

--- src/server.js
+++ src/server.js
@@ -12,12 +12,20 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:8080',
+    'http://localhost:5173',
+    'https://roomie-finder-ub.lovable.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/rooms", require("./routes/roomRoutes"));
+app.use("/api/classrooms", require("./routes/classroomRoutes"));
 app.use("/api/timetable", require("./routes/timetableRoutes"));
 app.use("/api/recommend", require("./routes/recommendRoutes"));
 
